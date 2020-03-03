6ee3799560075728c08737f852608178e85ba523
Mikael Lehto <mikael.leh@hotmail.com>
2020-03-03 17:59:17 +0200

    Error handling

--- src/index.ts
+++ src/index.ts
@@ -24,6 +24,20 @@ app.get("/", async (req: any, res: any) => {
             "/api/memo",
         ]
     }));
+
+app.use((req, res, next) => {
+    const error = new Error(`Not Found - ${req.originalUrl}`);
+    res.status(404);
+    next(error);
+});
+
+app.use((error: any, req: any, res: any, next: any) => {
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    res.status(statusCode);
+    res.json({
+        message: error.message,
+        stack: process.env.NODE_ENV === "production" ? "" : error.stack
+    });
 });
 
 // start the Express server

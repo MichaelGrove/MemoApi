e004c18da5ff6d16624e63b7e3cc2eb703fcba23
Mikael Lehto <mikael.leh@hotmail.com>
2020-03-20 21:09:29 +0200

    Remove escape to not break links

--- src/validators/memo.ts
+++ src/validators/memo.ts
@@ -2,7 +2,7 @@ import { check } from "express-validator";
 
 export default [
     check("title", "'Title' is required").isString().trim().escape().not().isEmpty(),
-    check("message", "'Message' is required").isString().trim().escape().not().isEmpty(),
+    check("message", "'Message' is required").isString().trim().not().isEmpty(),
     check("isFavourite", "'Is Favourite' needs to be a number").isInt(),
     check("isHidden", "'Is Hidden' needs to be a number").isInt(),
     check("categories", "'Categories' needs to be an array").isArray().custom((cids) => {
